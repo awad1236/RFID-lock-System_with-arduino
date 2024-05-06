097366b4c4f625e90b036c800799e162ce3e4606
Mohamed_Awad <152029138+awad1236@users.noreply.github.com>
2024-05-06 12:03:17 +0300

    Update lock.ino

--- lock.ino
+++ lock.ino
@@ -11,13 +11,6 @@
 const String UID = "73 6B 31 BE";
 const String password = "1234";
 byte lock = 1;
-char ctr;
-char ctr_wrong;
-char time;
-String enteredPassword = "";
-bool flag = true;
-char i;
-String ID;
 
 Servo servo;
 LiquidCrystal_I2C lcd(0x27, 16, 2);
@@ -25,8 +18,7 @@ MFRC522 rfid(SS_PIN, RST_PIN);
 
 const byte rows[4] = { 5, 6, 7, 8 };
 const byte cols[4] = { A0, A1, A2, A3 };
-char keys[4][4] = 
-{
+char keys[4][4] = {
 
   { 'D', 'C', 'B', 'A' },
   { '#', '9', '6', '3' },
@@ -46,22 +38,36 @@ byte klb[] =
 };
 
 Keypad mykeypad = Keypad(makeKeymap(keys), rows, cols, 4, 4);
+char ctr;
+char ctr_wrong;
+char time = 2;
+String enteredPassword = "";
+bool flag = true;
+char i;
+int pos=0;
+bool flg=true;
+String ID;
+void setup() {
 
-void setup() 
-{
-  Serial.begin(9600);
-  servo.write(70);
-  lcd.init();
-  lcd.backlight();
-  servo.attach(3);
-  SPI.begin();
-  rfid.PCD_Init();
-  pinMode(4, OUTPUT);
-  lcd.createChar(10, klb);
+    Serial.begin(9600);
+    lcd.init();
+    lcd.backlight();
+    servo.attach(3);
+    SPI.begin();
+    rfid.PCD_Init();
+    pinMode(4, OUTPUT);
+    lcd.createChar(10, klb);
 }
 
-void loop()
- {
+void loop() {
+  if(flg)
+  {
+     for (pos = 10; pos <= 160; pos += 1) { 
+    servo.write(pos);             
+    delay(13);  
+    flg=false;                     
+  }
+  }
   flag = true;
   lcd.setCursor(3, 0);
   lcd.print("Welcome");
@@ -85,10 +91,9 @@ void loop()
 }
 
 
-void read_card() /* reading the id of the card */
+void read_card() /*reading the id of the card*/
 {
-  for (byte i = 0; i < rfid.uid.size; i++) 
-  {
+  for (byte i = 0; i < rfid.uid.size; i++) {
     lcd.print(".");
     ID.concat(String(rfid.uid.uidByte[i] < 0x10 ? " 0" : " "));
     ID.concat(String(rfid.uid.uidByte[i], HEX));
@@ -96,17 +101,13 @@ void read_card() /* reading the id of the card */
   }
   ID.toUpperCase();
 }
-void check_card() /* check if the card is the right one */
+void check_card() /*check if the card is the right one*/
 {
-  if (ID.substring(1) == UID && lock == 0) 
-  {
+  if (ID.substring(1) == UID && lock == 0) {
     look_door();
-  } 
-  else if (ID.substring(1) == UID && lock == 1) 
-  {
+  } else if (ID.substring(1) == UID && lock == 1) {
     open_door();
-  } 
-  else {
+  } else {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Wrong card!");
@@ -128,9 +129,11 @@ void check_card() /* check if the card is the right one */
     }
   }
 }
-void open_door(void) /* open door function */
-{ 
-  servo.write(160);
+void open_door(void) { /*open door function*/
+  for (pos = 160; pos >= 10; pos -= 1) { 
+    servo.write(pos);              
+    delay(13);                      
+  }
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Door is open");
@@ -142,14 +145,15 @@ void open_door(void) /* open door function */
   lock = 0;
   ctr = 0;
 }
-void look_door() /* look door function */
-{ 
-  servo.write(70);
+void look_door() { /*look door function*/
+  for (pos = 10; pos <= 160; pos += 1) { 
+    servo.write(pos);              
+    delay(10);                   
+  }
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Door is locked");
-  for (int i = 0; i < 2; i++) 
-  {
+  for (int i = 0; i < 2; i++) {
     digitalWrite(4, 1);
     delay(50);
     digitalWrite(4, 0);
@@ -160,10 +164,12 @@ void look_door() /* look door function */
   lock = 1;
   ctr = 0;
 }
-void e7ramy() /* Alarm bell function */
-{ 
-  for (char i = time; i > 0; i--) 
-  {
+void e7ramy() { /*Alarm bell function*/
+ for (pos = 10; pos <= 160; pos += 1) { 
+    servo.write(pos);         
+    delay(13);                   
+  }
+  for (int i = time; i > 0; i--) {
     if (ctr_wrong != 3 && !flag) break;
     delay(500);
     lcd.clear();
@@ -171,15 +177,12 @@ void e7ramy() /* Alarm bell function */
     lcd.print("emsk 7ramaaaaai");
     lcd.setCursor(7, 1);
 
-    if (i < 10) 
-    {
+    if (i < 10) {
       lcd.setCursor(8, 1);
       lcd.print(i);
       lcd.setCursor(7, 1);
       lcd.print(0);
-    } 
-    else 
-    {
+    } else {
       lcd.print(i);
     }
     digitalWrite(4, 1);
@@ -188,15 +191,13 @@ void e7ramy() /* Alarm bell function */
 
     lcd.clear();
     lcd.setCursor(0, 0);
-    if (i == 1) 
-    {
+    if (i == 1) {
       ctr_wrong = 0;
       flag = false;
     }
   }
 }
-void delet_char() /* deleting last char of the user input using 'D' */
-{ 
+void delet_char() { /*deleting last char of the user input using 'D'*/
 
   int length = enteredPassword.length();
   enteredPassword.remove(i - 1, 1);
@@ -204,27 +205,24 @@ void delet_char() /* deleting last char of the user input using 'D' */
   lcd.print("Enter Password:");
   lcd.setCursor(0, 1);
   i--;
-  for (char j = 0; j < i; j++) {
+  for (int j = 0; j < i; j++) {
     lcd.print('*');
   }
 }
-void getPass() /*getting password from the user*/
-{ 
+void getPass() { /*getting password from the user*/
   enteredPassword = "";
   i = 0;
-  while (enteredPassword.length() < 4) 
-  {
+  while (enteredPassword.length() < 4) {
     char key = mykeypad.getKey();
-    if (key != NO_KEY) 
-    {
+    if (key != NO_KEY) {
       digitalWrite(4, 1);
       delay(50);
       digitalWrite(4, 0);
-      if (key == 'D') 
-      {
+      if (key == 'D') {
         delet_char();
         continue;
       }
+      // if(key == 'A')break;
       enteredPassword += key;
       Serial.println(key);
       Serial.println(enteredPassword);
@@ -234,28 +232,13 @@ void getPass() /*getting password from the user*/
     }
   }
 }
-void check_pass() /*check if the password is correct or wrong*/
-{ 
-  if (enteredPassword == password) 
-  {
+void check_pass() { /*check if the password is correct or wrong*/
+  if (enteredPassword == password) {
 
     lcd.print("Password Correct");
     delay(1500);
-    servo.write(160);
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Door is open");
-    digitalWrite(4, 1);
-    delay(40);
-    digitalWrite(4, 0);
-    delay(1500);
-    lcd.clear();
-    lock = 0;
-    ctr = 0;
-    flag = true;
-  } 
-  else 
-  {
+  open_door();
+  } else {
     lcd.print("Wrong Password!");
     ctr_wrong++;
     delay(1500);
